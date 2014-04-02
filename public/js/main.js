c63331a3bd433736be80c2bd83ac476db5673237
Luce <Luce@172-26-40-35.dynapool.nyu.edu>
2014-04-02 16:59:50 -0400

    added more jquery

--- public/js/main.js
+++ public/js/main.js
@@ -6,9 +6,58 @@ $( document ).ready(function() {
 	    console.log("you've been clicked");
 	    event.preventDefault();
 	});	
+	$(".alert").hide();
+	
 });
 
 function someFunction() {
-    alert( "The link will no longer take you to jquery.com" );
+//    alert( "The link will no longer take you to jquery.com" );
     console.log("somefuntion");
 }
+
+//This form will request a certain landfill by name
+jQuery("form").submit(function(e){	
+  	// the name form field value
+  	var entry = jQuery("#formEntry").val();
+  	$(".alert").show().fadeOut(3000);
+//  	$(".alert").alert("close");	  	
+  	console.log("Form submitted with value :" + entry);
+
+  	// This is taking the information and posting it to the OpenTrashLab Api.  This fails because the url is mean to GET data....
+/*
+  	$.ajax({
+	  	type: "POST",
+	  	url: "http://www.opentrashlab.com/landfills/data",
+	  	dataType: "json",
+	  	data: {
+		  	landfill : landfill
+	  	},
+       	success : function(response){
+    		// success - for now just log it
+    		console.log("Success : " + response);
+
+    	},
+    	error : function(err){
+    		// do error checking
+    		alert("something went wrong");
+    		console.error(err);
+    	}
+	  	
+  	})
+*/
+/*
+	$.ajax({
+		type: "GET",
+		url: "http://www.opentrashlab.com/landfills/data",
+		dataType: "json",
+		success: function(data) {processData(data)},
+       	error : function(err){
+			// do error checking
+			alert("something went wrong");
+			console.error(err);
+       	}
+	});
+*/  
+
+    e.preventDefault();
+});
